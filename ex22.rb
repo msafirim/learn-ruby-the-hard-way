02d7403e808f8671c6ae94cc70ba6a01b349c010
Moses Msafiri <mssmsfr@gmail.com>
2013-12-10 18:53:38 +0300

    Add what known so far

---
+++ ex22.rb
@@ -0,0 +1,13 @@
+def # define - create/define function 
+end # end function
+puts " " # print results/output with automatically newline
+gets.chomp() # get user input
+STDIN.gets.chomp() # get user input in arguments 
+STDIN.gets #
+puts "#{}" # '#{}' - shows variable in output
+prompt '' # ask user something / user input
+print " " # print output without newline
+ARGV  # argument variable - holds the arguments passed in scripts when they're run
+*args # arguments (any) - same as ARGV
+\n # newline
+File.open() # open File
