fed1be237cc35336d0ea0d3215a6c9caf8bd712a
Moses Msafiri <mssmsfr@gmail.com>
2013-12-12 18:02:31 +0300

    Add to show composition of classes

--- ex44.rb
+++ ex44.rb
@@ -58,3 +58,42 @@ son = Child.new()
 
 dad.altered()
 son.altered()
+
+
+class Other
+
+    def override()
+        puts "OTHER override()"
+    end
+
+    def implicit()
+        puts "OTHER implicit()"
+    end
+
+    def altered()
+        puts "OTHER altered()"
+    end
+end
+
+class Child
+
+    def initialize()
+        @other = Other.new()
+    end
+
+    def implicit()
+        @other.implicit()
+    end
+    
+    def override()
+        puts "CHILD override()"
+    end
+
+    def altered()
+        puts "CHILD, BEFORE OTHER altered()"
+        @other.altered()
+        puts "CHILD, AFTER OTHER altered()"
+    end
+end
+
+son = Child.new()
