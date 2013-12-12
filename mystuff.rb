931a3bf0c55db60e3d544b71937f6f9c0af868a4
Moses Msafiri <mssmsfr@gmail.com>
2013-12-12 13:36:30 +0300

    Add variable to a module

--- mystuff.rb
+++ mystuff.rb
@@ -3,7 +3,6 @@ module MyStuff
     def MyStuff.apple()
         puts "I AM APPLES!"
     end
-
-     # this is just a variable
+    # this is just a variable
     TANGERINE = "Living reflection of a dream"
 end
