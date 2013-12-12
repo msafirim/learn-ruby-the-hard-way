f2a5cecb9bcd68766008daefedbe65159a20c792
Moses Msafiri <mssmsfr@gmail.com>
2013-12-12 17:48:08 +0300

    Add override explicitly

--- ex44.rb
+++ ex44.rb
@@ -13,3 +13,24 @@ son = Child.new()
 
 dad.implicit()
 son.implicit()
+
+
+class Parent
+
+    def override()
+        puts "PARENT override()"
+    end
+end
+
+class Child < Parent
+    
+    def override()
+        puts "CHILD override()"
+    end
+end
+
+dad = Parent.new()
+son = Child.new()
+
+dad.override()
+son.override()
