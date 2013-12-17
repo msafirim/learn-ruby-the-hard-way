de235923123dd92c8b0353c91a77adb35c13e214
Moses Msafiri <mssmsfr@gmail.com>
2013-12-17 15:35:51 +0300

    Modify gothonweb.rb

--- projects/gothonweb/lib/gothonweb.rb
+++ projects/gothonweb/lib/gothonweb.rb
@@ -1,5 +1,20 @@
-require "gothonweb/version"
+# gem 'sinatra', '1.3.6'
+require_relative "gothonweb/version"
+require "sinatra"
+require "erb"
 
 module Gothonweb
-  # Your code goes here...
+  get '/' do
+   greeting = "Hello, World!"
+   erb :index, :locals => {:greeting => greeting}
+  end
+
+  get '/hello' do
+    erb :hello_form
+  end
+
+  post '/hello' do
+    greeting = "#{params[:greet] || "Hello"}, #{params[:name] || "Nobody"}"
+    erb :index, :locals => {:greeting => greeting}
+  end
 end
